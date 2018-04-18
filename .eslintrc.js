c9077a7ab3b7712f5fdfa595c6eaf21d97d7e8bd
王玲波 <454949876@qq.com>
2018-04-18 13:57:20 +0800

    eslint文件更新规范

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,4 @@
 // https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
   parser: 'babel-eslint',
@@ -11,17 +10,29 @@ module.exports = {
   },
   // https://github.com/standard/standard/blob/master/docs/RULES-en.md
   extends: 'standard',
-  // required to lint *.vue files
   plugins: [
     'html'
   ],
-  // add your custom rules here
   'rules': {
-    // allow paren-less arrow functions
     'arrow-parens': 0,
-    // allow async-await
     'generator-star-spacing': 0,
-    // allow debugger during development
+    'camelcase': [0, {
+      'properties': 'always'
+    }],
+    'indent': [2, 2, {
+      'SwitchCase': 1
+    }],
+    'space-before-function-paren': [2, 'never'],
+    'spaced-comment': [2, 'always', {
+      'markers': ['global', 'globals', 'eslint', 'eslint-disable', '*package', '!', ',']
+    }],
+    'no-new-object': 2,
+    'no-new-require': 2,
+    'no-new-symbol': 2,
+    'no-new-wrappers': 2,
+    'no-multiple-empty-lines': [2, {
+      'max': 1
+    }],
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
