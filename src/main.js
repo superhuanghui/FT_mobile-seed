57f747fa273ed3168cc51c3eefbe9ba1ae2a2dd2
王玲波 <454949876@qq.com>
2018-04-18 18:37:39 +0800

    增加是否需要登录模块的设置

--- src/main.js
+++ src/main.js
@@ -8,6 +8,9 @@ FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
+// 是否需要登录
+let needLogin = false
+
 const vm = new Vue({
   el: '#app-box',
   router,
@@ -16,3 +19,18 @@ const vm = new Vue({
 })
 
 Vue.use(vm)
+
+const urlSearchParams = {}
+if (location.search.indexOf('?') !== -1) {
+  const searchArr = location.search.substr(1).split('&')
+  for (let i = 0; i < searchArr.length; i++) {
+    if (searchArr[i].split('=')[1]) {
+      urlSearchParams[searchArr[i].split('=')[0]] = unescape(searchArr[i].split('=')[1])
+    }
+  }
+}
+
+if (!urlSearchParams.sessionId && needLogin) {
+  const fromUrlPath = window.location.origin
+  window.location.href = 'http://www.mdguanjia.com/activities/login/login.html?resource=h5&fromUrlPath=' + fromUrlPath
+}
